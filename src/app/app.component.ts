8119eedb000b80e2fee71a1305712a46aec4f4f4
Wallace Coelho <wac.almeida@gmail.com>
2017-09-23 09:37:03 -0300

    Initiate Core Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { CoreComponent } from './core/core.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  entryComponents: [CoreComponent]
 })
 export class AppComponent {
-  title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
+import { CoreComponent } from './core/core.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    CoreComponent
   ],
   imports: [
     BrowserModule
