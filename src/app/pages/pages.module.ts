de651c3a243b862d7f29a9fad2d727abc8da76d2
Wallace Coelho <wac.almeida@gmail.com>
2017-09-25 09:46:02 -0300

    Remove wrong imports

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,9 +6,6 @@ import { CartComponent } from './cart/cart.component';
 import { ProductsModule } from '../products/products.module';
 import { SharedModule } from '../shared/shared.module';
 
-import { ProductCardComponent } from '../products/product-card/product-card.component';
-import { ProductGridComponent } from '../products/product-grid/product-grid.component';
-
 
 @NgModule({
   id: module.id,
@@ -20,10 +17,6 @@ import { ProductGridComponent } from '../products/product-grid/product-grid.comp
   declarations: [
     HomeComponent, 
     CartComponent,
-  ],
-  entryComponents: [
-    ProductCardComponent,
-    ProductGridComponent
   ]
 })
 export class PagesModule { }
