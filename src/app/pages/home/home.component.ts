598564546b8d65d535bd25e37b1d9b50c7e72705
Wallace Coelho <wac.almeida@gmail.com>
2017-09-25 10:08:13 -0300

    Remove Unused imports on Home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { CartService } from '../../products/shared/cart-service/cart.service';
 import { ProductGridComponent } from '../../products/product-grid/product-grid.component';
